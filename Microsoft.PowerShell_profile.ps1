498de5525ef5e4be9a293f2f55e6902c46f9cd24
Alexander-Poganatz <alex.poga@outlook.com>
2024-11-13 19:14:48 -0500

    Added some git functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,4 +13,10 @@ function Glimpse-File {
     Get-Content $FileName -TotalCount $RowCount | foreach { if ($_.Length -le $ColW) { $_ } else { $_.SubString(0, $ColW)}  }
 }
 
+Function Get-BranchName { return (git branch --show-current) }
+
+Function Git-WordDiff { git diff --word-diff=color $args }
+
+Function Git-Log { git log -n 10 --reverse --oneline $args }
+
 oh-my-posh init pwsh --config "$HOME\Documents\PowerShell\reimagined-octo-sniffle.minimal.omp.json" | Invoke-Expression
